fb7183d650b41f8ee5e5aab43b6adb2201312892
StarBeasts <ridleyevan60@yahoo.com>
2023-10-13 19:59:00 -0400

    REJECT MODERNITY, RETURN TO LAWNCRETE

--- data/pokemon/base_stats/ivysaur.asm
+++ data/pokemon/base_stats/ivysaur.asm
@@ -1,16 +1,16 @@
 	db DEX_IVYSAUR ; pokedex id
 
-	db  60,  62,  63,  60,  80
+	db  85,  95,  105,  40,  50
 	;   hp  atk  def  spd  spc
 
-	db GRASS, POISON ; type
+	db GRASS, FIGHTING; type
 	db 45 ; catch rate
 	db 141 ; base exp
 
 	INCBIN "gfx/pokemon/front/ivysaur.pic", 0, 1 ; sprite dimensions
 	dw IvysaurPicFront, IvysaurPicBack
 
-	db TACKLE, GROWL, LEECH_SEED, NO_MOVE ; level 1 learnset
+	db COMET_PUNCH, HARDEN, LEECH_SEED, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_SLOW ; growth rate
 
 	; tm/hm learnset
