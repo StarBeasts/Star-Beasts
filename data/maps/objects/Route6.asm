b2bb51a7910c7f08e6720fbc4b754e346c2fdb71
StarBeasts <ridleyevan60@yahoo.com>
2024-06-23 18:45:43 -0400

    route 6, saffron city, vermilion and viridian checked

--- data/maps/objects/Route6.asm
+++ data/maps/objects/Route6.asm
@@ -3,7 +3,7 @@ Route6_Object:
 
 	def_warp_events
 	warp_event  9,  1, ROUTE_6_GATE, 3
-	warp_event 10,  1, ROUTE_6_GATE, 3
+	warp_event 10,  1, ROUTE_6_GATE, 4
 	warp_event 10,  7, ROUTE_6_GATE, 1
 	warp_event 17, 13, UNDERGROUND_PATH_ROUTE_6, 1
 
