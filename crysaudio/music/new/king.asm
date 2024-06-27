8304bf0162e5f127bf9c36a594c6edfa4521fe35
StarBeasts <ridleyevan60@yahoo.com>
2024-06-27 12:29:14 -0400

    new music, maps for 3.0

---
+++ crysaudio/music/new/king.asm
@@ -0,0 +1,1480 @@
+;Coverted using MIDI2ASM
+;Code by TriteHexagon
+;Version 5.2.0 (16-Nov-2022)
+;Visit github.com/TriteHexagon/Midi2ASM-Converter for up-to-date versions.
+
+; ============================================================================================================
+
+Music_King:
+	channel_count 4
+	channel 1, Music_King_Ch1
+	channel 2, Music_King_Ch2
+	channel 3, Music_King_Ch3
+	channel 4, Music_King_Ch4
+
+Music_King_Ch1:
+	volume 7, 7
+	duty_cycle 1
+	note_type 12, 10, 7
+	tempo 212
+;Bar 1
+	rest 16
+;Bar 2
+	octave 4
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 3
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note G_, 4
+;Bar 4
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 5
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note C_, 3
+	rest 16
+	rest 16
+	rest 16
+	rest 13
+;Bar 9
+	note C_, 1
+	note D_, 1
+	note E_, 1
+	note F_, 1
+;Bar 10
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 11
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note G_, 4
+;Bar 12
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 13
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note C_, 2
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 1
+;Bar 14
+	note D_, 2
+	note G_, 2
+	note G_, 2
+	note D_, 2
+	note E_, 2
+	note D_, 1
+	note C_, 1
+	note D_, 2
+	octave 3
+	note B_, 2
+;Bar 15
+	octave 4
+	note G_, 2
+	note F_, 1
+	note E_, 1
+	note F_, 2
+	note D_, 2
+	note E_, 2
+	note F_, 2
+	note G_, 2
+	note A_, 2
+;Bar 16
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 17
+	note F#, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 3
+	note A_, 4
+	note A_, 1
+	note A_, 1
+	note A_, 1
+	note A_, 1
+;Bar 18
+	octave 5
+	note D_, 2
+	note C_, 1
+	octave 4
+	note B_, 1
+	octave 5
+	note C_, 2
+	octave 4
+	note A_, 2
+	note B_, 2
+	note G_, 2
+	note D_, 2
+	note G_, 2
+;Bar 19
+	note A_, 2
+	note B_, 1
+	octave 5
+	note C_, 1
+	octave 4
+	note B_, 2
+	note A_, 2
+	note G_, 2
+	note E_, 2
+	note D_, 4
+;Bar 20
+	octave 5
+	note D_, 2
+	note C_, 1
+	octave 4
+	note B_, 1
+	octave 5
+	note C_, 2
+	octave 4
+	note A_, 2
+	note B_, 2
+	note G_, 2
+	note D_, 2
+	note G_, 2
+;Bar 21
+	note A_, 2
+	octave 5
+	note C_, 2
+	octave 4
+	note B_, 2
+	note A_, 2
+	note G_, 7
+	rest 1
+;Bar 22
+	octave 5
+	note G_, 2
+	note F_, 1
+	note E_, 1
+	note F_, 2
+	note D_, 2
+	note E_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	octave 5
+	note C_, 2
+;Bar 23
+	note D_, 2
+	note E_, 1
+	note F_, 1
+	note E_, 2
+	note D_, 2
+	note C_, 2
+	octave 4
+	note A_, 2
+	note G_, 4
+;Bar 24
+	octave 5
+	note G_, 2
+	note F_, 1
+	note E_, 1
+	note F_, 2
+	note D_, 2
+	note E_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	octave 5
+	note C_, 2
+;Bar 25
+	note D_, 2
+	note F_, 2
+	note E_, 2
+	note D_, 2
+	note C_, 1
+	octave 4
+	note B_, 1
+	octave 5
+	note C_, 1
+	note D_, 1
+	note E_, 4
+;Bar 26
+	octave 4
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 27
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note G_, 4
+;Bar 28
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 29
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note C_, 3
+	rest 16
+	rest 16
+	rest 16
+	rest 13
+;Bar 33
+	note C_, 1
+	note D_, 1
+	note E_, 1
+	note F_, 1
+;Bar 34
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 35
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note G_, 4
+;Bar 36
+	note G_, 2
+	octave 5
+	note C_, 2
+	note C_, 2
+	octave 4
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 37
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D_, 2
+	note C_, 2
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 1
+	sound_ret
+
+; ============================================================================================================
+
+Music_King_Ch2:
+	duty_cycle 2
+	note_type 12, 10, 7
+;Bar 1
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 14
+;Bar 5
+	octave 2
+	volume_envelope 8, 7
+	note G_, 1
+	note A_, 1
+;Bar 6
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+;Bar 7
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+	octave 3
+	note G_, 2
+	note F_, 1
+	note E_, 1
+	note D_, 1
+	note E_, 1
+	note D_, 1
+	note C_, 1
+;Bar 8
+	octave 2
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+;Bar 9
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+	octave 3
+	note G_, 4
+	note F_, 1
+	note E_, 2
+	note F_, 1
+;Bar 10
+	note G_, 2
+	octave 4
+	note C_, 2
+	note C_, 2
+	octave 3
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 11
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D_, 2
+	note G_, 4
+;Bar 12
+	note G_, 2
+	octave 4
+	note C_, 2
+	note C_, 2
+	octave 3
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 13
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D_, 2
+	note C_, 2
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 1
+;Bar 14
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+;Bar 18
+	octave 1
+	volume_envelope 5, 7
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+;Bar 19
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+;Bar 20
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+;Bar 21
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+	octave 1
+	note G_, 1
+	octave 2
+	note G_, 1
+;Bar 22
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+;Bar 23
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+;Bar 24
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+;Bar 25
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note C_, 1
+	octave 2
+	note C_, 1
+;Bar 26
+	rest 16
+	rest 16
+	rest 16
+	rest 14
+;Bar 29
+	volume_envelope 8, 7
+	note G_, 1
+	note A_, 1
+;Bar 30
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+;Bar 31
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+	octave 3
+	note G_, 2
+	note F_, 1
+	note E_, 1
+	note D_, 1
+	note E_, 1
+	note D_, 1
+	note C_, 1
+;Bar 32
+	octave 2
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+;Bar 33
+	note B_, 2
+	octave 3
+	note C_, 1
+	note D_, 1
+	octave 2
+	note B_, 2
+	note G_, 2
+	octave 3
+	note G_, 4
+	note F_, 1
+	note E_, 2
+	note F_, 1
+;Bar 34
+	note G_, 2
+	octave 4
+	note C_, 2
+	note C_, 2
+	octave 3
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 35
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D_, 2
+	note G_, 4
+;Bar 36
+	note G_, 2
+	octave 4
+	note C_, 2
+	note C_, 2
+	octave 3
+	note G_, 2
+	note A_, 2
+	note G_, 1
+	note F_, 1
+	note G_, 2
+	note E_, 2
+;Bar 37
+	note F_, 2
+	note E_, 1
+	note D_, 1
+	note E_, 2
+	note C_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D_, 2
+	note C_, 2
+	octave 2
+	note B_, 1
+	octave 3
+	note C_, 16
+	note C_, 1
+	sound_ret
+
+; ============================================================================================================
+
+Music_King_Ch3:
+	note_type 12, 1, 0
+;Bar 1
+	octave 3
+	volume_envelope 1, 0
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 2
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 3
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+;Bar 4
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 5
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 6
+	octave 2
+	note G_, 6
+	note G_, 2
+	note G_, 6
+	note G_, 2
+;Bar 7
+	note G_, 6
+	note G_, 2
+	note G_, 4
+	note G_, 4
+;Bar 8
+	note G_, 6
+	note G_, 2
+	note G_, 6
+	note G_, 2
+;Bar 9
+	note G_, 6
+	note G_, 2
+	note G_, 4
+	note G_, 4
+;Bar 10
+	octave 3
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 11
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+;Bar 12
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 13
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 1
+;Bar 18
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+;Bar 19
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+;Bar 20
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+;Bar 21
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G_, 2
+	octave 3
+	note D_, 2
+;Bar 22
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+;Bar 23
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+;Bar 24
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+;Bar 25
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+	note C_, 2
+	note G_, 2
+;Bar 26
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 27
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+;Bar 28
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 29
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 30
+	octave 2
+	note G_, 6
+	note G_, 2
+	note G_, 6
+	note G_, 2
+;Bar 31
+	note G_, 6
+	note G_, 2
+	note G_, 4
+	note G_, 4
+;Bar 32
+	note G_, 6
+	note G_, 2
+	note G_, 6
+	note G_, 2
+;Bar 33
+	note G_, 6
+	note G_, 2
+	note G_, 4
+	note G_, 4
+;Bar 34
+	octave 3
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 35
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+;Bar 36
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+;Bar 37
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 1
+	rest 1
+	octave 2
+	note G_, 1
+	rest 1
+	octave 3
+	note D_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note G_, 16
+	note G_, 2
+	sound_ret
+
+; ============================================================================================================
+
+Music_King_Ch4:
+	toggle_noise 1
+	drum_speed 12
+;Bar 1
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+	rest 16
+;Bar 18
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+;Bar 19
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+;Bar 20
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+;Bar 21
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+;Bar 22
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+;Bar 23
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+;Bar 24
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+;Bar 25
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+	drum_note 6, 2
+	drum_note 8, 2
+;Bar 26
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	drum_note 6, 16
+	sound_ret
+
+; ============================================================================================================
+
